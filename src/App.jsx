fd1a2abd565784b083dd589d9673ed68e13bf43f
Ali Naqvi <alibnaqvi851@gmail.com>
2024-10-29 16:14:14 -0700

    Step 2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import BaristaForm from './Components/baristaForm'
 
 function App() {
-  return (
-    <>
-      <h1>Oh My Grind</h1>
-    </>
-  )
+    return (
+        <>
+        <div className="title-container">
+            <h1 className="title">Oh My Grind</h1>
+            <p>So you think you can barista? Let's put that to the test...</p>
+        </div>
+        <BaristaForm />
+        </>
+    )
 }
 
 export default App
